62304f2d299f44061be2630896e33178822c58d6
saurabhkanswal <saurabhkanswal8101@gmail.com>
2021-09-22 16:18:44 +0530

    First Commit

--- app/App.js
+++ app/App.js
@@ -1,84 +1,106 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from "react";
 import {
-	SafeAreaView,
-	StyleSheet,
-	Text,
-	View,
-	TouchableOpacity
-} from 'react-native';
-import _ from 'lodash';
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  ScrollView,
+} from "react-native";
+import _ from "lodash";
 
-import Card from './Card';
+import Card from "./Card";
 
-export default class App extends Component {
-	render() {
-		return (
-      <SafeAreaView style={styles.container}>
-        <View style={styles.container}>
-          <View style={styles.column}>
-            <View style={styles.columnContent}>
-              <Card num={1} />
+export default App = () => {
+  const [totalColumn, setTotalColumn] = useState([[], []]);
 
-              <Card num={2} />
+  // const [columnFirstHeight, setColumnFirstHeight] = useState(0);
 
-              <Card num={3} />
-            </View>
+  console.log(totalColumn);
 
-            <TouchableOpacity style={styles.addBtn}>
-              <Text style={styles.addBtnText}>Add</Text>
-            </TouchableOpacity>
-          </View>
+  const addNewCard = ({ index, columnIndex }) => {
+    let newArr = [...totalColumn];
+    newArr[columnIndex].push(index);
+    setTotalColumn([...newArr]);
+  };
 
-          <View style={styles.column}>
-            <View style={styles.columnContent}>
-              <Card num={1} />
+  const addNewCloumn = () => {
+    setTotalColumn((prevState) => [...prevState, []]);
+  };
 
-              <Card num={2} />
-            </View>
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.topContainer}>
+        <ScrollView horizontal style={{ flex: 1 }}>
+          {totalColumn.map((item, index) => {
+            return (
+              <View style={styles.column} key={index}>
+                <View style={{ flex: 1 }}>
+                  {item.length > 0 &&
+                    item.map((item, index) => {
+                      return <Card num={item} key={index} />;
+                    })}
+                </View>
+                <Pressable
+                  style={styles.addBtn}
+                  onPress={() =>
+                    addNewCard({
+                      index: item.length + 1,
+                      columnIndex: index,
+                    })
+                  }
+                >
+                  <Text style={styles.addBtnText}>Add</Text>
+                </Pressable>
+              </View>
+            );
+          })}
+        </ScrollView>
+        <Pressable style={styles.addBtn} onPress={() => addNewCloumn()}>
+          <Text style={styles.addBtnText}>Add</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // justifyContent: "center",
 
-            <TouchableOpacity style={styles.addBtn}>
-              <Text style={styles.addBtnText}>Add</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </SafeAreaView>
-		);
-	}
-}
+    alignItems: "stretch",
+    backgroundColor: "#f2f2f2",
+    padding: 8,
+    paddingTop: 20,
+    flexDirection: "row",
+  },
 
-const styles = StyleSheet.create( {
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'stretch',
-		backgroundColor: '#f2f2f2',
-		padding: 8,
-		paddingTop: 20,
-		flexDirection: 'row'
-	},
-
-	column: {
-		flex: 1,
-		justifyContent: 'flex-start',
-		alignItems: 'stretch',
-		backgroundColor: '#FFF',
-		padding: 8,
-		margin: 8
-	},
-	columnContent: {
-		flex: 1,
+  column: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+    backgroundColor: "#FFF",
+    padding: 8,
+    margin: 8,
+    width: 200,
+  },
+  columnContent: {
+    flex: 1,
   },
   addBtn: {
     height: 50,
-    backgroundColor: '#412243',
+    backgroundColor: "#412243",
     marginTop: 12,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 8
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
   },
   addBtnText: {
-    color: '#fff',
-    fontWeight: 'bold',
+    color: "#fff",
+    fontWeight: "bold",
     fontSize: 20,
-  }
-} );
+  },
+  topContainer: {
+    flex: 1,
+  },
+});
